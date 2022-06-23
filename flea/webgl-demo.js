8efcb8a93b92c5e184185debd4c484426512c971
engbJapan <39077511+engbJapan@users.noreply.github.com>
2022-06-23 10:20:21 +0900

    Update webgl-demo.js

--- flea/webgl-demo.js
+++ flea/webgl-demo.js
@@ -89,7 +89,7 @@ function main() {
   // objects we'll be drawing.
   const buffers = initBuffers(gl);
 
-  const texture = loadTexture(gl, './cubetexture.png');
+  const texture = loadTexture(gl, 'cubetexture.png');
 
   var then = 0;
 
